e57f7513d64d56d28f650f00087e17d3f375d41b
AreaLiang <137739760@qq.com>
2022-03-31 09:29:03 +0800

    1

---
+++ src/views/businessEntrust/bsEntrust.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="">
+		<!-- 主题内容顶部 -->
+		<PageHeader :breadcrumbItem="$route.meta.headName" />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'bsEntrust', //业务委托
+		data() {
+			return {
+				
+			}
+		},
+		components: {
+
+		},
+		mounted() {
+		
+		}
+	}
+</script>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/views/entrustTbDownload/entrustTbDL.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="">
+		<h3>666</h3>
+	</div>
+</template>
+
+<script>
+	
+	export default {
+		name: 'entrustTbDL',//业务委托表格下载
+		data() {
+			return {
+
+			}
+		},
+		components: {
+
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	
+</style>
